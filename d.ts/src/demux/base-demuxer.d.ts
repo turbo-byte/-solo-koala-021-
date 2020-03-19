a4e43d6c43613759746a3fe3309394b4049018f2
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2020-03-19 22:57:34 +0000

    Added en_CA to ALL_LINGUAS. Added Canadian English translation.

--- d.ts/src/demux/base-demuxer.d.ts
+++ d.ts/src/demux/base-demuxer.d.ts
@@ -1,2 +1,5 @@
 import MediaInfo from '../core/media-info';
-import { PESPrivateData, PESPrivateDataDescri
+import { PESPrivateData, PESPrivateDataDescriptor } from './pes-private-data';
+import { SMPTE2038Data } from './smpte2038';
+import { SCTE35Data } from './scte35';
+declare type OnErrorCallback = (type: s
