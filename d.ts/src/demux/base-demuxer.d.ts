becc60ac603a968d7aeea901281cc34e2145cc2e
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2020-03-21 10:45:21 +0000

    Fix test config

--- d.ts/src/demux/base-demuxer.d.ts
+++ d.ts/src/demux/base-demuxer.d.ts
@@ -2,4 +2,7 @@ import MediaInfo from '../core/media-info';
 import { PESPrivateData, PESPrivateDataDescriptor } from './pes-private-data';
 import { SMPTE2038Data } from './smpte2038';
 import { SCTE35Data } from './scte35';
-declare type OnErrorCallback = (type: s
+declare type OnErrorCallback = (type: string, info: string) => void;
+declare type OnMediaInfoCallback = (mediaInfo: MediaInfo) => void;
+declare type OnMetaDataArrivedCallback = (metadata: any) => void;
+declare type OnTrackMetadata
