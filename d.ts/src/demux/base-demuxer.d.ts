c980c0d811f0dab9e1197c2c2b9df9f9f925f36e
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2020-03-23 19:52:11 +0000

    warnings--

--- d.ts/src/demux/base-demuxer.d.ts
+++ d.ts/src/demux/base-demuxer.d.ts
@@ -7,4 +7,6 @@ declare type OnMediaInfoCallback = (mediaInfo: MediaInfo) => void;
 declare type OnMetaDataArrivedCallback = (metadata: any) => void;
 declare type OnTrackMetadataCallback = (type: string, metadata: any) => void;
 declare type OnDataAvailableCallback = (videoTrack: any, audioTrack: any) => void;
-declare type O
+declare type OnTimedID3MetadataCallback = (timed_id3_data: PESPrivateData) => void;
+declare type OnSMPTE2038MetadataCallback = (smpte2038_data: SMPTE2038Data) => void;
+declare type OnSCTE35MetadataCallback = (scte35_dat
