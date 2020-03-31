15aed85acb823c0e5b9b24f678984f8c5b4bb170
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2020-03-31 12:06:14 +0000

    Update cn.md

--- d.ts/src/demux/h264.d.ts
+++ d.ts/src/demux/h264.d.ts
@@ -1,4 +1,14 @@
 export declare enum H264NaluType {
     kUnspecified = 0,
     kSliceNonIDR = 1,
-    kSliceDPA
+    kSliceDPA = 2,
+    kSliceDPB = 3,
+    kSliceDPC = 4,
+    kSliceIDR = 5,
+    kSliceSEI = 6,
+    kSliceSPS = 7,
+    kSlicePPS = 8,
+    kSliceAUD = 9,
+    kEndOfSequence = 10,
+    kEndOfStream = 11,
+    
