e4a37f284370eff74c081154d4b2b77c660658aa
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2020-04-17 17:18:47 +0000

    commit by jayli

--- d.ts/src/demux/pat-pmt-pes.d.ts
+++ d.ts/src/demux/pat-pmt-pes.d.ts
@@ -1,1 +1,9 @@
-interface ProgramToPMTPIDMap 
+interface ProgramToPMTPIDMap {
+    [program: number]: number;
+}
+export declare class PAT {
+    version_number: number;
+    network_pid: number;
+    program_pmt_pid: ProgramToPMTPIDMap;
+}
+export declare enum StreamT
