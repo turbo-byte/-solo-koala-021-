af7ae0341b3727b30a8799e65cb9318f5a2e618c
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2020-04-17 13:37:55 +0000

    add linter

--- d.ts/src/demux/mpeg4-audio.d.ts
+++ d.ts/src/demux/mpeg4-audio.d.ts
@@ -1,1 +1,12 @@
-export declare enu
+export declare enum MPEG4AudioObjectTypes {
+    kNull = 0,
+    kAACMain = 1,
+    kAAC_LC = 2,
+    kAAC_SSR = 3,
+    kAAC_LTP = 4,
+    kAAC_SBR = 5,
+    kAAC_Scalable = 6,
+    kLayer1 = 32,
+    kLayer2 = 33,
+    kLayer3 = 34
+}
