ca9b736701387a9c716cadd2b05d26b76f5b175d
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2020-04-21 12:11:10 +0000

    Update Chinese simplified translation

--- d.ts/src/demux/pes-private-data.d.ts
+++ d.ts/src/demux/pes-private-data.d.ts
@@ -1,4 +1,12 @@
 export declare class PESPrivateData {
     pid: number;
     stream_id: number;
-    pts
+    pts?: number;
+    dts?: number;
+    nearest_pts?: number;
+    data: Uint8Array;
+    len: number;
+}
+export declare class PESPrivateDataDescriptor {
+    pid: number;
+    stream_
