57c2ce72292362fdd989a6bdbb3b5daff261ec7b
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2020-04-21 16:44:13 +0000

    fix unicode error

--- d.ts/src/demux/ts-demuxer.d.ts
+++ d.ts/src/demux/ts-demuxer.d.ts
@@ -1,2 +1,9 @@
 import BaseDemuxer from './base-demuxer';
-declare class TSDemuxer ext
+declare class TSDemuxer extends BaseDemuxer {
+    private readonly TAG;
+    private config_;
+    private ts_packet_size_;
+    private sync_offset_;
+    private first_parse_;
+    private media_info_;
+    private times
