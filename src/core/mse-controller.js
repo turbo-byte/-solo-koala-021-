64395350de265b4845af31a872da2028f352db35
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2020-08-19 09:30:28 +0000

    Minor tidying up

--- src/core/mse-controller.js
+++ src/core/mse-controller.js
@@ -1,2 +1,7 @@
 /*
- * Copyright (C) 201
+ * Copyright (C) 2016 Bilibili. All Rights Reserved.
+ *
+ * @author zheng qian <xqq@xqq.im>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in
