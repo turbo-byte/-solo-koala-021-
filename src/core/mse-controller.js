7c4c1f8d4e0348e47e775e79130a055a8d398b06
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2020-08-20 15:41:02 +0000

    Updated the neptune-client feedstock.

--- src/core/mse-controller.js
+++ src/core/mse-controller.js
@@ -12,4 +12,11 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import EventEmitter from 'events';
+import Log from '../utils/logger.js';
+import Browser from '../utils/browser.js';
+import MSEEvents fro
