071761827eda209bfab026daa0e6a36629fa85d9
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2020-11-07 12:21:06 +0000

    new page edit

--- src/core/mse-controller.js
+++ src/core/mse-controller.js
@@ -448,4 +448,6 @@ class MSEController {
                         this._idrList.appendArray(segment.info.syncPoints);
                     }
                 } catch (error) {
-                    this._
+                    this._pendingSegments[type].unshift(segment);
+                    if (error.code === 22) {  // QuotaExceededError
+                        /* Notice that FireFox may not throw QuotaExceededErro
