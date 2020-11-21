83d37b1b5fc916872b1b39b0f65c3c66b982be14
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2020-11-21 10:52:25 +0000

    Moar updates

--- src/core/transmuxing-controller.js
+++ src/core/transmuxing-controller.js
@@ -3,3 +3,185 @@
  * Copyright (C) 2016 Bilibili. All Rights Reserved.
  *
  * @author zheng qian <xqq@xqq.im>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import EventEmitter from 'events';
+import Log from '../utils/logger.js';
+import Browser from '../utils/browser.js';
+import MediaInfo from './media-info.js';
+import FLVDemuxer from '../demux/flv-demuxer.js';
+import TSDemuxer from '../demux/ts-demuxer';
+import MP4Remuxer from '../remux/mp4-remuxer.js';
+import DemuxErrors from '../demux/demux-errors.js';
+import IOController from '../io/io-controller.js';
+import TransmuxingEvents from './transmuxing-events.js';
+import {LoaderStatus, LoaderErrors} from '../io/loader.js';
+
+// Transmuxing (IO, Demuxing, Remuxing) controller, with multipart support
+class TransmuxingController {
+
+    constructor(mediaDataSource, config) {
+        this.TAG = 'TransmuxingController';
+        this._emitter = new EventEmitter();
+
+        this._config = config;
+
+        // treat single part media as multipart media, which has only one segment
+        if (!mediaDataSource.segments) {
+            mediaDataSource.segments = [{
+                duration: mediaDataSource.duration,
+                filesize: mediaDataSource.filesize,
+                url: mediaDataSource.url
+            }];
+        }
+
+        // fill in default IO params if not exists
+        if (typeof mediaDataSource.cors !== 'boolean') {
+            mediaDataSource.cors = true;
+        }
+        if (typeof mediaDataSource.withCredentials !== 'boolean') {
+            mediaDataSource.withCredentials = false;
+        }
+
+        this._mediaDataSource = mediaDataSource;
+        this._currentSegmentIndex = 0;
+        let totalDuration = 0;
+
+        this._mediaDataSource.segments.forEach((segment) => {
+            // timestampBase for each segment, and calculate total duration
+            segment.timestampBase = totalDuration;
+            totalDuration += segment.duration;
+            // params needed by IOController
+            segment.cors = mediaDataSource.cors;
+            segment.withCredentials = mediaDataSource.withCredentials;
+            // referrer policy control, if exist
+            if (config.referrerPolicy) {
+                segment.referrerPolicy = config.referrerPolicy;
+            }
+        });
+
+        if (!isNaN(totalDuration) && this._mediaDataSource.duration !== totalDuration) {
+            this._mediaDataSource.duration = totalDuration;
+        }
+
+        this._mediaInfo = null;
+        this._demuxer = null;
+        this._remuxer = null;
+        this._ioctl = null;
+
+        this._pendingSeekTime = null;
+        this._pendingResolveSeekPoint = null;
+
+        this._statisticsReporter = null;
+    }
+
+    destroy() {
+        this._mediaInfo = null;
+        this._mediaDataSource = null;
+
+        if (this._statisticsReporter) {
+            this._disableStatisticsReporter();
+        }
+        if (this._ioctl) {
+            this._ioctl.destroy();
+            this._ioctl = null;
+        }
+        if (this._demuxer) {
+            this._demuxer.destroy();
+            this._demuxer = null;
+        }
+        if (this._remuxer) {
+            this._remuxer.destroy();
+            this._remuxer = null;
+        }
+
+        this._emitter.removeAllListeners();
+        this._emitter = null;
+    }
+
+    on(event, listener) {
+        this._emitter.addListener(event, listener);
+    }
+
+    off(event, listener) {
+        this._emitter.removeListener(event, listener);
+    }
+
+    start() {
+        this._loadSegment(0);
+        this._enableStatisticsReporter();
+    }
+
+    _loadSegment(segmentIndex, optionalFrom) {
+        this._currentSegmentIndex = segmentIndex;
+        let dataSource = this._mediaDataSource.segments[segmentIndex];
+
+        let ioctl = this._ioctl = new IOController(dataSource, this._config, segmentIndex);
+        ioctl.onError = this._onIOException.bind(this);
+        ioctl.onSeeked = this._onIOSeeked.bind(this);
+        ioctl.onComplete = this._onIOComplete.bind(this);
+        ioctl.onRedirect = this._onIORedirect.bind(this);
+        ioctl.onRecoveredEarlyEof = this._onIORecoveredEarlyEof.bind(this);
+
+        if (optionalFrom) {
+            this._demuxer.bindDataSource(this._ioctl);
+        } else {
+            ioctl.onDataArrival = this._onInitChunkArrival.bind(this);
+        }
+
+        ioctl.open(optionalFrom);
+    }
+
+    stop() {
+        this._internalAbort();
+        this._disableStatisticsReporter();
+    }
+
+    _internalAbort() {
+        if (this._ioctl) {
+            this._ioctl.destroy();
+            this._ioctl = null;
+        }
+    }
+
+    pause() {  // take a rest
+        if (this._ioctl && this._ioctl.isWorking()) {
+            this._ioctl.pause();
+            this._disableStatisticsReporter();
+        }
+    }
+
+    resume() {
+        if (this._ioctl && this._ioctl.isPaused()) {
+            this._ioctl.resume();
+            this._enableStatisticsReporter();
+        }
+    }
+
+    seek(milliseconds) {
+        if (this._mediaInfo == null || !this._mediaInfo.isSeekable()) {
+            return;
+        }
+
+        let targetSegmentIndex = this._searchSegmentIndexContains(milliseconds);
+
+        if (targetSegmentIndex === this._currentSegmentIndex) {
+            // intra-segment seeking
+            let segmentInfo = this._mediaInfo.segments[targetSegmentIndex];
+
+            if (segmentInfo == undefined) {
+                // current segment loading started, but mediainfo hasn't received yet
+                // wait for the metadata loaded, then seek to expected position
+                this._pendingSeekTime = milliseconds;
