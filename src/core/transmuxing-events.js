e2efccfd3dd00cbaa80dd0abfdf56468eeb37ded
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2020-11-27 08:13:03 +0000

    Tests working

--- src/core/transmuxing-events.js
+++ src/core/transmuxing-events.js
@@ -19,4 +19,9 @@
 const TransmuxingEvents = {
     IO_ERROR: 'io_error',
     DEMUX_ERROR: 'demux_error',
-    INIT_SEGMENT: 'init_segm
+    INIT_SEGMENT: 'init_segment',
+    MEDIA_SEGMENT: 'media_segment',
+    LOADING_COMPLETE: 'loading_complete',
+    RECOVERED_EARLY_EOF: 'recovered_early_eof',
+    MEDIA_INFO: 'media_info',
+    METADATA_ARRIVED: 'metadata_arrived
