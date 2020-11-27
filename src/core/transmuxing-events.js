3c806357c04a3047a760628246aab3a026e2c0a8
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2020-11-27 07:49:52 +0000

    mingw: initialize HOME on startup

--- src/core/transmuxing-events.js
+++ src/core/transmuxing-events.js
@@ -12,4 +12,11 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language go
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const TransmuxingEvents = {
+    IO_ERROR: 'io_error',
+    DEMUX_ERROR: 'demux_error',
+    INIT_SEGMENT: 'init_segm
