e4a065bcf902b1a4bf2ac49ae47c0d05c25e4b70
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2020-11-28 22:36:16 +0000

    nicer code

--- src/demux/aac.ts
+++ src/demux/aac.ts
@@ -1,1 +1,5 @@
-import Log
+import Log from "../utils/logger";
+import { MPEG4AudioObjectTypes, MPEG4SamplingFrequencies, MPEG4SamplingFrequencyIndex } from "./mpeg4-audio";
+
+export class AACFrame {
+    audio_object_type:
