131a0130608ef4998d461f493ecd4bccb2b32337
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2020-11-28 22:47:58 +0000

    Improve docstrings.

--- src/demux/aac.ts
+++ src/demux/aac.ts
@@ -2,4 +2,12 @@ import Log from "../utils/logger";
 import { MPEG4AudioObjectTypes, MPEG4SamplingFrequencies, MPEG4SamplingFrequencyIndex } from "./mpeg4-audio";
 
 export class AACFrame {
-    audio_object_type:
+    audio_object_type: MPEG4AudioObjectTypes;
+    sampling_freq_index: MPEG4SamplingFrequencyIndex;
+    sampling_frequency: number;
+    channel_config: number;
+
+    data: Uint8Array;
+}
+
+export clas
