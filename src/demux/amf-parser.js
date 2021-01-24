d764f7dd2e6a63fee9fffef0beeb1c3c43453493
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2021-01-24 06:43:42 +0000

    Add license headers

--- src/demux/amf-parser.js
+++ src/demux/amf-parser.js
@@ -21,3 +21,7 @@ import decodeUTF8 from '../utils/utf8-conv.js';
 import {IllegalStateException} from '../utils/exception.js';
 
 let le = (function () {
+    let buf = new ArrayBuffer(2);
+    (new DataView(buf)).setInt16(0, 256, true);  // little-endian write
+    return (new Int16Array(buf))[0] === 256;  // platform-spec read, if equal then LE
+})();
