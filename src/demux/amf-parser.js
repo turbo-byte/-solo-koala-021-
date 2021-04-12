daf892a6a46b9e9c6397b62618876784932ab1ba
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2021-04-12 18:15:20 +0000

    tcp: do not forget FIN in tcp_shifted_skb()

--- src/demux/amf-parser.js
+++ src/demux/amf-parser.js
@@ -156,4 +156,5 @@ class AMF {
                     if ((v.getUint32(dataSize - 4, !le) & 0x00FFFFFF) === 9) {
                         terminal = 3;
                     }
-                    while (offset < dataSi
+                    while (offset < dataSize - 4) {  // 4 === type(UI8) + ScriptDataObjectEnd(UI24)
+                        let amfobj = AMF.parseObject(arrayBuffer, dataOffset + offset, dataSize - offset - terminal);
