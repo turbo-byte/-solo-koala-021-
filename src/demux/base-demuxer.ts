633b978321c3021205f5a8795b7a23bad0cf56b2
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2021-05-23 23:08:31 +0000

    clean version

--- src/demux/base-demuxer.ts
+++ src/demux/base-demuxer.ts
@@ -4,4 +4,6 @@ import { SMPTE2038Data } from './smpte2038';
 import { SCTE35Data } from './scte35';
 
 type OnErrorCallback = (type: string, info: string) => void;
-type OnM
+type OnMediaInfoCallback = (mediaInfo: MediaInfo) => void;
+type OnMetaDataArrivedCallback = (metadata: any) => void;
+type OnTrackMetadataCallback = (type: string, metadata: any) => vo
