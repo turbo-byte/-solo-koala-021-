b116246139eff708e293bcc8c2c7fa0a6939a3fd
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2021-05-23 10:58:43 +0000

    Added FIXME

--- src/demux/base-demuxer.ts
+++ src/demux/base-demuxer.ts
@@ -1,2 +1,7 @@
 import MediaInfo from '../core/media-info';
-import { PESPrivateData, PESPriva
+import { PESPrivateData, PESPrivateDataDescriptor } from './pes-private-data';
+import { SMPTE2038Data } from './smpte2038';
+import { SCTE35Data } from './scte35';
+
+type OnErrorCallback = (type: string, info: string) => void;
+type OnM
