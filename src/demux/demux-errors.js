c69d7e4094b010279b1aeceb5ef6ea366179e71a
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2021-06-20 17:29:44 +0000

    Automated merge with ssh://team@pocoo.org/pygments-main

--- src/demux/demux-errors.js
+++ src/demux/demux-errors.js
@@ -12,4 +12,15 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permi
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const DemuxErrors = {
+    OK: 'OK',
+    FORMAT_ERROR: 'FormatError',
+    FORMAT_UNSUPPORTED: 'FormatUnsupported',
+    CODEC_UNSUPPORTED: 'CodecUnsupported'
+};
+
+expo
