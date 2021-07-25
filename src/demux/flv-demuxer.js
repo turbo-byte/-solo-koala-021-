cd84b518d84ec4168cc167f0f40533327025b4af
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2021-07-25 08:15:29 +0000

    Bump version & changelog

--- src/demux/flv-demuxer.js
+++ src/demux/flv-demuxer.js
@@ -39,3 +39,215 @@ function Swap32(src) {
 }
 
 function ReadBig32(array, index) {
+    return ((array[index] << 24)     |
+            (array[index + 1] << 16) |
+            (array[index + 2] << 8)  |
+            (array[index + 3]));
+}
+
+
+class FLVDemuxer {
+
+    constructor(probeData, config) {
+        this.TAG = 'FLVDemuxer';
+
+        this._config = config;
+
+        this._onError = null;
+        this._onMediaInfo = null;
+        this._onMetaDataArrived = null;
+        this._onScriptDataArrived = null;
+        this._onTrackMetadata = null;
+        this._onDataAvailable = null;
+
+        this._dataOffset = probeData.dataOffset;
+        this._firstParse = true;
+        this._dispatch = false;
+
+        this._hasAudio = probeData.hasAudioTrack;
+        this._hasVideo = probeData.hasVideoTrack;
+
+        this._hasAudioFlagOverrided = false;
+        this._hasVideoFlagOverrided = false;
+
+        this._audioInitialMetadataDispatched = false;
+        this._videoInitialMetadataDispatched = false;
+
+        this._mediaInfo = new MediaInfo();
+        this._mediaInfo.hasAudio = this._hasAudio;
+        this._mediaInfo.hasVideo = this._hasVideo;
+        this._metadata = null;
+        this._audioMetadata = null;
+        this._videoMetadata = null;
+
+        this._naluLengthSize = 4;
+        this._timestampBase = 0;  // int32, in milliseconds
+        this._timescale = 1000;
+        this._duration = 0;  // int32, in milliseconds
+        this._durationOverrided = false;
+        this._referenceFrameRate = {
+            fixed: true,
+            fps: 23.976,
+            fps_num: 23976,
+            fps_den: 1000
+        };
+
+        this._flvSoundRateTable = [5500, 11025, 22050, 44100, 48000];
+
+        this._mpegSamplingRates = [
+            96000, 88200, 64000, 48000, 44100, 32000,
+            24000, 22050, 16000, 12000, 11025, 8000, 7350
+        ];
+
+        this._mpegAudioV10SampleRateTable = [44100, 48000, 32000, 0];
+        this._mpegAudioV20SampleRateTable = [22050, 24000, 16000, 0];
+        this._mpegAudioV25SampleRateTable = [11025, 12000, 8000,  0];
+
+        this._mpegAudioL1BitRateTable = [0, 32, 64, 96, 128, 160, 192, 224, 256, 288, 320, 352, 384, 416, 448, -1];
+        this._mpegAudioL2BitRateTable = [0, 32, 48, 56,  64,  80,  96, 112, 128, 160, 192, 224, 256, 320, 384, -1];
+        this._mpegAudioL3BitRateTable = [0, 32, 40, 48,  56,  64,  80,  96, 112, 128, 160, 192, 224, 256, 320, -1];
+
+        this._videoTrack = {type: 'video', id: 1, sequenceNumber: 0, samples: [], length: 0};
+        this._audioTrack = {type: 'audio', id: 2, sequenceNumber: 0, samples: [], length: 0};
+
+        this._littleEndian = (function () {
+            let buf = new ArrayBuffer(2);
+            (new DataView(buf)).setInt16(0, 256, true);  // little-endian write
+            return (new Int16Array(buf))[0] === 256;  // platform-spec read, if equal then LE
+        })();
+    }
+
+    destroy() {
+        this._mediaInfo = null;
+        this._metadata = null;
+        this._audioMetadata = null;
+        this._videoMetadata = null;
+        this._videoTrack = null;
+        this._audioTrack = null;
+
+        this._onError = null;
+        this._onMediaInfo = null;
+        this._onMetaDataArrived = null;
+        this._onScriptDataArrived = null;
+        this._onTrackMetadata = null;
+        this._onDataAvailable = null;
+    }
+
+    static probe(buffer) {
+        let data = new Uint8Array(buffer);
+        if (data.byteLength < 9) {
+            return {needMoreData: true};
+        }
+
+        let mismatch = {match: false};
+
+        if (data[0] !== 0x46 || data[1] !== 0x4C || data[2] !== 0x56 || data[3] !== 0x01) {
+            return mismatch;
+        }
+
+        let hasAudio = ((data[4] & 4) >>> 2) !== 0;
+        let hasVideo = (data[4] & 1) !== 0;
+
+        let offset = ReadBig32(data, 5);
+
+        if (offset < 9) {
+            return mismatch;
+        }
+
+        return {
+            match: true,
+            consumed: offset,
+            dataOffset: offset,
+            hasAudioTrack: hasAudio,
+            hasVideoTrack: hasVideo
+        };
+    }
+
+    bindDataSource(loader) {
+        loader.onDataArrival = this.parseChunks.bind(this);
+        return this;
+    }
+
+    // prototype: function(type: string, metadata: any): void
+    get onTrackMetadata() {
+        return this._onTrackMetadata;
+    }
+
+    set onTrackMetadata(callback) {
+        this._onTrackMetadata = callback;
+    }
+
+    // prototype: function(mediaInfo: MediaInfo): void
+    get onMediaInfo() {
+        return this._onMediaInfo;
+    }
+
+    set onMediaInfo(callback) {
+        this._onMediaInfo = callback;
+    }
+
+    get onMetaDataArrived() {
+        return this._onMetaDataArrived;
+    }
+
+    set onMetaDataArrived(callback) {
+        this._onMetaDataArrived = callback;
+    }
+
+    get onScriptDataArrived() {
+        return this._onScriptDataArrived;
+    }
+
+    set onScriptDataArrived(callback) {
+        this._onScriptDataArrived = callback;
+    }
+
+    // prototype: function(type: number, info: string): void
+    get onError() {
+        return this._onError;
+    }
+
+    set onError(callback) {
+        this._onError = callback;
+    }
+
+    // prototype: function(videoTrack: any, audioTrack: any): void
+    get onDataAvailable() {
+        return this._onDataAvailable;
+    }
+
+    set onDataAvailable(callback) {
+        this._onDataAvailable = callback;
+    }
+
+    // timestamp base for output samples, must be in milliseconds
+    get timestampBase() {
+        return this._timestampBase;
+    }
+
+    set timestampBase(base) {
+        this._timestampBase = base;
+    }
+
+    get overridedDuration() {
+        return this._duration;
+    }
+
+    // Force-override media duration. Must be in milliseconds, int32
+    set overridedDuration(duration) {
+        this._durationOverrided = true;
+        this._duration = duration;
+        this._mediaInfo.duration = duration;
+    }
+
+    // Force-override audio track present flag, boolean
+    set overridedHasAudio(hasAudio) {
+        this._hasAudioFlagOverrided = true;
+        this._hasAudio = hasAudio;
+        this._mediaInfo.hasAudio = hasAudio;
+    }
+
+    // Force-override video track present flag, boolean
+    set overridedHasVideo(hasVideo) {
+        this._hasVideoFlagOverrided = true;
+        this._hasVideo = hasVideo;
