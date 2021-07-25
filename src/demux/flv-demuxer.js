00ad00075b11a175536d2920bb57ef0e0296bc9e
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2021-07-25 14:48:54 +0000

    nojekyll

--- src/demux/flv-demuxer.js
+++ src/demux/flv-demuxer.js
@@ -251,3 +251,212 @@ class FLVDemuxer {
     set overridedHasVideo(hasVideo) {
         this._hasVideoFlagOverrided = true;
         this._hasVideo = hasVideo;
+        this._mediaInfo.hasVideo = hasVideo;
+    }
+
+    resetMediaInfo() {
+        this._mediaInfo = new MediaInfo();
+    }
+
+    _isInitialMetadataDispatched() {
+        if (this._hasAudio && this._hasVideo) {  // both audio & video
+            return this._audioInitialMetadataDispatched && this._videoInitialMetadataDispatched;
+        }
+        if (this._hasAudio && !this._hasVideo) {  // audio only
+            return this._audioInitialMetadataDispatched;
+        }
+        if (!this._hasAudio && this._hasVideo) {  // video only
+            return this._videoInitialMetadataDispatched;
+        }
+        return false;
+    }
+
+    // function parseChunks(chunk: ArrayBuffer, byteStart: number): number;
+    parseChunks(chunk, byteStart) {
+        if (!this._onError || !this._onMediaInfo || !this._onTrackMetadata || !this._onDataAvailable) {
+            throw new IllegalStateException('Flv: onError & onMediaInfo & onTrackMetadata & onDataAvailable callback must be specified');
+        }
+
+        let offset = 0;
+        let le = this._littleEndian;
+
+        if (byteStart === 0) {  // buffer with FLV header
+            if (chunk.byteLength > 13) {
+                let probeData = FLVDemuxer.probe(chunk);
+                offset = probeData.dataOffset;
+            } else {
+                return 0;
+            }
+        }
+
+        if (this._firstParse) {  // handle PreviousTagSize0 before Tag1
+            this._firstParse = false;
+            if (byteStart + offset !== this._dataOffset) {
+                Log.w(this.TAG, 'First time parsing but chunk byteStart invalid!');
+            }
+
+            let v = new DataView(chunk, offset);
+            let prevTagSize0 = v.getUint32(0, !le);
+            if (prevTagSize0 !== 0) {
+                Log.w(this.TAG, 'PrevTagSize0 !== 0 !!!');
+            }
+            offset += 4;
+        }
+
+        while (offset < chunk.byteLength) {
+            this._dispatch = true;
+
+            let v = new DataView(chunk, offset);
+
+            if (offset + 11 + 4 > chunk.byteLength) {
+                // data not enough for parsing an flv tag
+                break;
+            }
+
+            let tagType = v.getUint8(0);
+            let dataSize = v.getUint32(0, !le) & 0x00FFFFFF;
+
+            if (offset + 11 + dataSize + 4 > chunk.byteLength) {
+                // data not enough for parsing actual data body
+                break;
+            }
+
+            if (tagType !== 8 && tagType !== 9 && tagType !== 18) {
+                Log.w(this.TAG, `Unsupported tag type ${tagType}, skipped`);
+                // consume the whole tag (skip it)
+                offset += 11 + dataSize + 4;
+                continue;
+            }
+
+            let ts2 = v.getUint8(4);
+            let ts1 = v.getUint8(5);
+            let ts0 = v.getUint8(6);
+            let ts3 = v.getUint8(7);
+
+            let timestamp = ts0 | (ts1 << 8) | (ts2 << 16) | (ts3 << 24);
+
+            let streamId = v.getUint32(7, !le) & 0x00FFFFFF;
+            if (streamId !== 0) {
+                Log.w(this.TAG, 'Meet tag which has StreamID != 0!');
+            }
+
+            let dataOffset = offset + 11;
+
+            switch (tagType) {
+                case 8:  // Audio
+                    this._parseAudioData(chunk, dataOffset, dataSize, timestamp);
+                    break;
+                case 9:  // Video
+                    this._parseVideoData(chunk, dataOffset, dataSize, timestamp, byteStart + offset);
+                    break;
+                case 18:  // ScriptDataObject
+                    this._parseScriptData(chunk, dataOffset, dataSize);
+                    break;
+            }
+
+            let prevTagSize = v.getUint32(11 + dataSize, !le);
+            if (prevTagSize !== 11 + dataSize) {
+                Log.w(this.TAG, `Invalid PrevTagSize ${prevTagSize}`);
+            }
+
+            offset += 11 + dataSize + 4;  // tagBody + dataSize + prevTagSize
+        }
+
+        // dispatch parsed frames to consumer (typically, the remuxer)
+        if (this._isInitialMetadataDispatched()) {
+            if (this._dispatch && (this._audioTrack.length || this._videoTrack.length)) {
+                this._onDataAvailable(this._audioTrack, this._videoTrack);
+            }
+        }
+
+        return offset;  // consumed bytes, just equals latest offset index
+    }
+
+    _parseScriptData(arrayBuffer, dataOffset, dataSize) {
+        let scriptData = AMF.parseScriptData(arrayBuffer, dataOffset, dataSize);
+
+        if (scriptData.hasOwnProperty('onMetaData')) {
+            if (scriptData.onMetaData == null || typeof scriptData.onMetaData !== 'object') {
+                Log.w(this.TAG, 'Invalid onMetaData structure!');
+                return;
+            }
+            if (this._metadata) {
+                Log.w(this.TAG, 'Found another onMetaData tag!');
+            }
+            this._metadata = scriptData;
+            let onMetaData = this._metadata.onMetaData;
+
+            if (this._onMetaDataArrived) {
+                this._onMetaDataArrived(Object.assign({}, onMetaData));
+            }
+
+            if (typeof onMetaData.hasAudio === 'boolean') {  // hasAudio
+                if (this._hasAudioFlagOverrided === false) {
+                    this._hasAudio = onMetaData.hasAudio;
+                    this._mediaInfo.hasAudio = this._hasAudio;
+                }
+            }
+            if (typeof onMetaData.hasVideo === 'boolean') {  // hasVideo
+                if (this._hasVideoFlagOverrided === false) {
+                    this._hasVideo = onMetaData.hasVideo;
+                    this._mediaInfo.hasVideo = this._hasVideo;
+                }
+            }
+            if (typeof onMetaData.audiodatarate === 'number') {  // audiodatarate
+                this._mediaInfo.audioDataRate = onMetaData.audiodatarate;
+            }
+            if (typeof onMetaData.videodatarate === 'number') {  // videodatarate
+                this._mediaInfo.videoDataRate = onMetaData.videodatarate;
+            }
+            if (typeof onMetaData.width === 'number') {  // width
+                this._mediaInfo.width = onMetaData.width;
+            }
+            if (typeof onMetaData.height === 'number') {  // height
+                this._mediaInfo.height = onMetaData.height;
+            }
+            if (typeof onMetaData.duration === 'number') {  // duration
+                if (!this._durationOverrided) {
+                    let duration = Math.floor(onMetaData.duration * this._timescale);
+                    this._duration = duration;
+                    this._mediaInfo.duration = duration;
+                }
+            } else {
+                this._mediaInfo.duration = 0;
+            }
+            if (typeof onMetaData.framerate === 'number') {  // framerate
+                let fps_num = Math.floor(onMetaData.framerate * 1000);
+                if (fps_num > 0) {
+                    let fps = fps_num / 1000;
+                    this._referenceFrameRate.fixed = true;
+                    this._referenceFrameRate.fps = fps;
+                    this._referenceFrameRate.fps_num = fps_num;
+                    this._referenceFrameRate.fps_den = 1000;
+                    this._mediaInfo.fps = fps;
+                }
+            }
+            if (typeof onMetaData.keyframes === 'object') {  // keyframes
+                this._mediaInfo.hasKeyframesIndex = true;
+                let keyframes = onMetaData.keyframes;
+                this._mediaInfo.keyframesIndex = this._parseKeyframesIndex(keyframes);
+                onMetaData.keyframes = null;  // keyframes has been extracted, remove it
+            } else {
+                this._mediaInfo.hasKeyframesIndex = false;
+            }
+            this._dispatch = false;
+            this._mediaInfo.metadata = onMetaData;
+            Log.v(this.TAG, 'Parsed onMetaData');
+            if (this._mediaInfo.isComplete()) {
+                this._onMediaInfo(this._mediaInfo);
+            }
+        }
+
+        if (Object.keys(scriptData).length > 0) {
+            if (this._onScriptDataArrived) {
+                this._onScriptDataArrived(Object.assign({}, scriptData));
+            }
+        }
+    }
+
+    _parseKeyframesIndex(keyframes) {
+        let times = [];
+        let filepositions = [];
