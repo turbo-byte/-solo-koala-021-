93c6302c7c5aa5df29e2366ade0cfefaf68020c8
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2021-08-13 15:45:01 +0000

    v0.2.5

--- src/demux/h265-parser.js
+++ src/demux/h265-parser.js
@@ -1,2 +1,7 @@
 /*
- * Copyright (C) 2022 もにょてっく. All R
+ * Copyright (C) 2022 もにょてっく. All Rights Reserved.
+ *
+ * @author もにょ〜ん <monyone.teihen@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compl
