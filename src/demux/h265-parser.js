da8f124c6926ec7ffabe86e1b1c9149dbb5707d1
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2021-08-18 06:48:18 +0000

    for check style

--- src/demux/h265-parser.js
+++ src/demux/h265-parser.js
@@ -30,3 +30,14 @@ class H265NaluParser {
             if (i >= 2) {
                 // Unescape: Skip 0x03 after 00 00
                 if (src[i] === 0x03 && src[i - 1] === 0x00 && src[i - 2] === 0x00) {
+                    continue;
+                }
+            }
+            dst[dst_idx] = src[i];
+            dst_idx++;
+        }
+
+        return new Uint8Array(dst.buffer, 0, dst_idx);
+    }
+
+   
