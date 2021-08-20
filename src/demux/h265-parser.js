f1235ab592ccb61c83e15f1341386735b0fdb286
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2021-08-20 09:02:37 +0000

    readme upd

--- src/demux/h265-parser.js
+++ src/demux/h265-parser.js
@@ -84,4 +84,6 @@ class H265NaluParser {
         let general_profile_compatibility_flags_1 = gb.readByte();
         let general_profile_compatibility_flags_2 = gb.readByte();
         let general_profile_compatibility_flags_3 = gb.readByte();
- 
+        let general_profile_compatibility_flags_4 = gb.readByte();
+        let general_constraint_indicator_flags_1 = gb.readByte();
+        let general_constrai
