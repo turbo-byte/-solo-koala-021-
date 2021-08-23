0c43161628b83ce2bd9171671e93901e74cfd322
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2021-08-23 17:05:18 +0000

    Adjust spacing

--- src/demux/h265-parser.js
+++ src/demux/h265-parser.js
@@ -130,4 +130,6 @@ class H265NaluParser {
         let bit_depth_luma_minus8 = gb.readUEG();
         let bit_depth_chroma_minus8 = gb.readUEG();
         let log2_max_pic_order_cnt_lsb_minus4 = gb.readUEG();
-        let sub_layer_ordering_info
+        let sub_layer_ordering_info_present_flag = gb.readBool();
+        for (let i = sub_layer_ordering_info_present_flag ? 0 : max_sub_layers_minus1; i <= max_sub_layers_minus1; i++) {
+            gb.readUEG(); // max_dec_pic_b
