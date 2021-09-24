c047d30f97f07d24b9c1432c54436f23bbf98192
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2021-09-24 20:40:03 +0000

    update php

--- src/demux/h265.ts
+++ src/demux/h265.ts
@@ -4,4 +4,16 @@ export enum H265NaluType {
     kSliceIDR_W_RADL = 19,
     kSliceIDR_N_LP = 20,
     kSliceCRA_NUT = 21,
-    kSliceVPS =
+    kSliceVPS = 32,
+    kSliceSPS = 33,
+    kSlicePPS = 34,
+    kSliceAUD = 35,
+}
+
+export class H265NaluPayload {
+    type: H265NaluType;
+    data: Uint8Array;
+}
+
+export class H265NaluHVC1 {
+    type:
