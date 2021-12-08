9e2d10d79b364dc2fa5dc0d83479b3fe2762e204
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2021-12-08 17:19:48 +0000

    working on transframe

--- src/io/io-controller.js
+++ src/io/io-controller.js
@@ -500,4 +500,6 @@ class IOController {
                     this._expandBuffer(this._stashUsed + chunk.byteLength);
                 }
                 let stashArray = new Uint8Array(this._stashBuffer, 0, this._bufferSize);
-           
+                stashArray.set(new Uint8Array(chunk), this._stashUsed);
+                this._stashUsed += chunk.byteLength;
+                let consumed = this._dispatchChunks(this
