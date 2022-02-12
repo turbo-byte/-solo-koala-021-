3da77f42f73e01a692bdbb762c5ce9b984c6fc4e
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2022-02-12 19:36:39 +0000

    d3 update

--- src/io/param-seek-handler.js
+++ src/io/param-seek-handler.js
@@ -19,4 +19,12 @@
 class ParamSeekHandler {
 
     constructor(paramStart, paramEnd) {
-    
+        this._startName = paramStart;
+        this._endName = paramEnd;
+    }
+
+    getConfig(baseUrl, range) {
+        let url = baseUrl;
+
+        if (range.from !== 0 || range.to !== -1) {
+   
