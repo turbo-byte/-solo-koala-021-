a587f575d5934e747192f27d8a7373602520b2c7
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2022-02-17 16:13:24 +0000

    Update online.md

--- src/io/range-seek-handler.js
+++ src/io/range-seek-handler.js
@@ -31,3 +31,23 @@ class RangeSeekHandler {
             if (range.to !== -1) {
                 param = `bytes=${range.from.toString()}-${range.to.toString()}`;
             } else {
+                param = `bytes=${range.from.toString()}-`;
+            }
+            headers['Range'] = param;
+        } else if (this._zeroStart) {
+            headers['Range'] = 'bytes=0-';
+        }
+
+        return {
+            url: url,
+            headers: headers
+        };
+    }
+
+    removeURLParameters(seekedURL) {
+        return seekedURL;
+    }
+
+}
+
+export default RangeSeekHandler;
