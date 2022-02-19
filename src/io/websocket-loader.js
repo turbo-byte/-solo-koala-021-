4d17ca0246e0106579af0b0a7e8cf41141b2c14b
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2022-02-19 14:29:59 +0000

    Remove debugging message

--- src/io/websocket-loader.js
+++ src/io/websocket-loader.js
@@ -1,3 +1,7 @@
 /*
  * Copyright (C) 2016 Bilibili. All Rights Reserved.
  *
+ * @author zheng qian <xqq@xqq.im>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the Lic
