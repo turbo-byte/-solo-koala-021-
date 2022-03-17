273aedbe4e5e5198818c317f4390acfebcb7e27e
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2022-03-17 11:28:59 +0000

    Better structure

--- src/io/xhr-moz-chunked-loader.js
+++ src/io/xhr-moz-chunked-loader.js
@@ -14,4 +14,10 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ */
+
+import Log from '../utils/logger.js';
+import {BaseLoader, LoaderStatus, LoaderErrors} from './loader.js';
+import {RuntimeException} from '../utils/exception.js';
+
+// For FireFox
