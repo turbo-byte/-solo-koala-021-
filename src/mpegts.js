6da1036b83f64f4ffafd0bac5d95b1e38e269c2e
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2022-04-24 08:58:23 +0000

    added portfolio

--- src/mpegts.js
+++ src/mpegts.js
@@ -9,4 +9,6 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITH
