4f1bb77dc8deb94fe4c6debafed58c073340508f
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2022-05-22 17:38:39 +0000

    amend last commit

--- src/player/mse-player.js
+++ src/player/mse-player.js
@@ -113,3 +113,182 @@ class MSEPlayer {
     }
 
     on(event, listener) {
+        if (event === PlayerEvents.MEDIA_INFO) {
+            if (this._mediaInfo != null) {
+                Promise.resolve().then(() => {
+                    this._emitter.emit(PlayerEvents.MEDIA_INFO, this.mediaInfo);
+                });
+            }
+        } else if (event === PlayerEvents.STATISTICS_INFO) {
+            if (this._statisticsInfo != null) {
+                Promise.resolve().then(() => {
+                    this._emitter.emit(PlayerEvents.STATISTICS_INFO, this.statisticsInfo);
+                });
+            }
+        }
+        this._emitter.addListener(event, listener);
+    }
+
+    off(event, listener) {
+        this._emitter.removeListener(event, listener);
+    }
+
+    attachMediaElement(mediaElement) {
+        this._mediaElement = mediaElement;
+        mediaElement.addEventListener('loadedmetadata', this.e.onvLoadedMetadata);
+        mediaElement.addEventListener('seeking', this.e.onvSeeking);
+        mediaElement.addEventListener('canplay', this.e.onvCanPlay);
+        mediaElement.addEventListener('stalled', this.e.onvStalled);
+        mediaElement.addEventListener('progress', this.e.onvProgress);
+
+        this._msectl = new MSEController(this._config);
+
+        this._msectl.on(MSEEvents.UPDATE_END, this._onmseUpdateEnd.bind(this));
+        this._msectl.on(MSEEvents.BUFFER_FULL, this._onmseBufferFull.bind(this));
+        this._msectl.on(MSEEvents.SOURCE_OPEN, () => {
+            this._mseSourceOpened = true;
+            if (this._hasPendingLoad) {
+                this._hasPendingLoad = false;
+                this.load();
+            }
+        });
+        this._msectl.on(MSEEvents.ERROR, (info) => {
+            this._emitter.emit(PlayerEvents.ERROR,
+                               ErrorTypes.MEDIA_ERROR,
+                               ErrorDetails.MEDIA_MSE_ERROR,
+                               info
+            );
+        });
+
+        this._msectl.attachMediaElement(mediaElement);
+
+        if (this._pendingSeekTime != null) {
+            try {
+                mediaElement.currentTime = this._pendingSeekTime;
+                this._pendingSeekTime = null;
+            } catch (e) {
+                // IE11 may throw InvalidStateError if readyState === 0
+                // We can defer set currentTime operation after loadedmetadata
+            }
+        }
+    }
+
+    detachMediaElement() {
+        if (this._mediaElement) {
+            this._msectl.detachMediaElement();
+            this._mediaElement.removeEventListener('loadedmetadata', this.e.onvLoadedMetadata);
+            this._mediaElement.removeEventListener('seeking', this.e.onvSeeking);
+            this._mediaElement.removeEventListener('canplay', this.e.onvCanPlay);
+            this._mediaElement.removeEventListener('stalled', this.e.onvStalled);
+            this._mediaElement.removeEventListener('progress', this.e.onvProgress);
+            this._mediaElement = null;
+        }
+        if (this._msectl) {
+            this._msectl.destroy();
+            this._msectl = null;
+        }
+    }
+
+    load() {
+        if (!this._mediaElement) {
+            throw new IllegalStateException('HTMLMediaElement must be attached before load()!');
+        }
+        if (this._transmuxer) {
+            throw new IllegalStateException('MSEPlayer.load() has been called, please call unload() first!');
+        }
+        if (this._hasPendingLoad) {
+            return;
+        }
+
+        if (this._config.deferLoadAfterSourceOpen && this._mseSourceOpened === false) {
+            this._hasPendingLoad = true;
+            return;
+        }
+
+        if (this._mediaElement.readyState > 0) {
+            this._requestSetTime = true;
+            // IE11 may throw InvalidStateError if readyState === 0
+            this._mediaElement.currentTime = 0;
+        }
+
+        this._transmuxer = new Transmuxer(this._mediaDataSource, this._config);
+
+        this._transmuxer.on(TransmuxingEvents.INIT_SEGMENT, (type, is) => {
+            this._msectl.appendInitSegment(is);
+        });
+        this._transmuxer.on(TransmuxingEvents.MEDIA_SEGMENT, (type, ms) => {
+            this._msectl.appendMediaSegment(ms);
+
+            // lazyLoad check
+            if (this._config.lazyLoad && !this._config.isLive) {
+                let currentTime = this._mediaElement.currentTime;
+                if (ms.info.endDts >= (currentTime + this._config.lazyLoadMaxDuration) * 1000) {
+                    if (this._progressChecker == null) {
+                        Log.v(this.TAG, 'Maximum buffering duration exceeded, suspend transmuxing task');
+                        this._suspendTransmuxer();
+                    }
+                }
+            }
+        });
+        this._transmuxer.on(TransmuxingEvents.LOADING_COMPLETE, () => {
+            this._msectl.endOfStream();
+            this._emitter.emit(PlayerEvents.LOADING_COMPLETE);
+        });
+        this._transmuxer.on(TransmuxingEvents.RECOVERED_EARLY_EOF, () => {
+            this._emitter.emit(PlayerEvents.RECOVERED_EARLY_EOF);
+        });
+        this._transmuxer.on(TransmuxingEvents.IO_ERROR, (detail, info) => {
+            this._emitter.emit(PlayerEvents.ERROR, ErrorTypes.NETWORK_ERROR, detail, info);
+        });
+        this._transmuxer.on(TransmuxingEvents.DEMUX_ERROR, (detail, info) => {
+            this._emitter.emit(PlayerEvents.ERROR, ErrorTypes.MEDIA_ERROR, detail, {code: -1, msg: info});
+        });
+        this._transmuxer.on(TransmuxingEvents.MEDIA_INFO, (mediaInfo) => {
+            this._mediaInfo = mediaInfo;
+            this._emitter.emit(PlayerEvents.MEDIA_INFO, Object.assign({}, mediaInfo));
+        });
+        this._transmuxer.on(TransmuxingEvents.METADATA_ARRIVED, (metadata) => {
+            this._emitter.emit(PlayerEvents.METADATA_ARRIVED, metadata);
+        });
+        this._transmuxer.on(TransmuxingEvents.SCRIPTDATA_ARRIVED, (data) => {
+            this._emitter.emit(PlayerEvents.SCRIPTDATA_ARRIVED, data);
+        });
+        this._transmuxer.on(TransmuxingEvents.TIMED_ID3_METADATA_ARRIVED, (timed_id3_metadata) => {
+            this._emitter.emit(PlayerEvents.TIMED_ID3_METADATA_ARRIVED, timed_id3_metadata);
+        });
+        this._transmuxer.on(TransmuxingEvents.SMPTE2038_METADATA_ARRIVED, (smpte2038_metadata) => {
+            this._emitter.emit(PlayerEvents.SMPTE2038_METADATA_ARRIVED, smpte2038_metadata);
+        });
+        this._transmuxer.on(TransmuxingEvents.SCTE35_METADATA_ARRIVED, (scte35_metadata) => {
+            this._emitter.emit(PlayerEvents.SCTE35_METADATA_ARRIVED, scte35_metadata);
+        });
+        this._transmuxer.on(TransmuxingEvents.PES_PRIVATE_DATA_DESCRIPTOR, (descriptor) => {
+            this._emitter.emit(PlayerEvents.PES_PRIVATE_DATA_DESCRIPTOR, descriptor);
+        });
+        this._transmuxer.on(TransmuxingEvents.PES_PRIVATE_DATA_ARRIVED, (private_data) => {
+            this._emitter.emit(PlayerEvents.PES_PRIVATE_DATA_ARRIVED, private_data);
+        });
+        this._transmuxer.on(TransmuxingEvents.STATISTICS_INFO, (statInfo) => {
+            this._statisticsInfo = this._fillStatisticsInfo(statInfo);
+            this._emitter.emit(PlayerEvents.STATISTICS_INFO, Object.assign({}, this._statisticsInfo));
+        });
+        this._transmuxer.on(TransmuxingEvents.RECOMMEND_SEEKPOINT, (milliseconds) => {
+            if (this._mediaElement && !this._config.accurateSeek) {
+                this._requestSetTime = true;
+                this._mediaElement.currentTime = milliseconds / 1000;
+            }
+        });
+
+        this._transmuxer.open();
+    }
+
+    unload() {
+        if (this._mediaElement) {
+            this._mediaElement.pause();
+        }
+        if (this._msectl) {
+            this._msectl.seek(0);
+        }
+        if (this._transmuxer) {
+            this._transmuxer.close();
+            this._transmuxer.destroy();
