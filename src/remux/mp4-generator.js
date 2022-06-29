d48261d2c2561c778829840fe221fd71ef15fa6b
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2022-06-29 06:08:03 +0000

    updated frontend

--- src/remux/mp4-generator.js
+++ src/remux/mp4-generator.js
@@ -6,3 +6,193 @@
  * @author zheng qian <xqq@xqq.im>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//  MP4 boxes generator for ISO BMFF (ISO Base Media File Format, defined in ISO/IEC 14496-12)
+class MP4 {
+
+    static init() {
+        MP4.types = {
+            avc1: [], avcC: [], btrt: [], dinf: [],
+            dref: [], esds: [], ftyp: [], hdlr: [],
+            hvc1: [], hvcC: [],
+            mdat: [], mdhd: [], mdia: [], mfhd: [],
+            minf: [], moof: [], moov: [], mp4a: [],
+            mvex: [], mvhd: [], sdtp: [], stbl: [],
+            stco: [], stsc: [], stsd: [], stsz: [],
+            stts: [], tfdt: [], tfhd: [], traf: [],
+            trak: [], trun: [], trex: [], tkhd: [],
+            vmhd: [], smhd: [], '.mp3': []
+        };
+
+        for (let name in MP4.types) {
+            if (MP4.types.hasOwnProperty(name)) {
+                MP4.types[name] = [
+                    name.charCodeAt(0),
+                    name.charCodeAt(1),
+                    name.charCodeAt(2),
+                    name.charCodeAt(3)
+                ];
+            }
+        }
+
+        let constants = MP4.constants = {};
+
+        constants.FTYP = new Uint8Array([
+            0x69, 0x73, 0x6F, 0x6D,  // major_brand: isom
+            0x0,  0x0,  0x0,  0x1,   // minor_version: 0x01
+            0x69, 0x73, 0x6F, 0x6D,  // isom
+            0x61, 0x76, 0x63, 0x31   // avc1
+        ]);
+
+        constants.STSD_PREFIX = new Uint8Array([
+            0x00, 0x00, 0x00, 0x00,  // version(0) + flags
+            0x00, 0x00, 0x00, 0x01   // entry_count
+        ]);
+
+        constants.STTS = new Uint8Array([
+            0x00, 0x00, 0x00, 0x00,  // version(0) + flags
+            0x00, 0x00, 0x00, 0x00   // entry_count
+        ]);
+
+        constants.STSC = constants.STCO = constants.STTS;
+
+        constants.STSZ = new Uint8Array([
+            0x00, 0x00, 0x00, 0x00,  // version(0) + flags
+            0x00, 0x00, 0x00, 0x00,  // sample_size
+            0x00, 0x00, 0x00, 0x00   // sample_count
+        ]);
+
+        constants.HDLR_VIDEO = new Uint8Array([
+            0x00, 0x00, 0x00, 0x00,  // version(0) + flags
+            0x00, 0x00, 0x00, 0x00,  // pre_defined
+            0x76, 0x69, 0x64, 0x65,  // handler_type: 'vide'
+            0x00, 0x00, 0x00, 0x00,  // reserved: 3 * 4 bytes
+            0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00,
+            0x56, 0x69, 0x64, 0x65,
+            0x6F, 0x48, 0x61, 0x6E,
+            0x64, 0x6C, 0x65, 0x72, 0x00  // name: VideoHandler
+        ]);
+
+        constants.HDLR_AUDIO = new Uint8Array([
+            0x00, 0x00, 0x00, 0x00,  // version(0) + flags
+            0x00, 0x00, 0x00, 0x00,  // pre_defined
+            0x73, 0x6F, 0x75, 0x6E,  // handler_type: 'soun'
+            0x00, 0x00, 0x00, 0x00,  // reserved: 3 * 4 bytes
+            0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00,
+            0x53, 0x6F, 0x75, 0x6E,
+            0x64, 0x48, 0x61, 0x6E,
+            0x64, 0x6C, 0x65, 0x72, 0x00  // name: SoundHandler
+        ]);
+
+        constants.DREF = new Uint8Array([
+            0x00, 0x00, 0x00, 0x00,  // version(0) + flags
+            0x00, 0x00, 0x00, 0x01,  // entry_count
+            0x00, 0x00, 0x00, 0x0C,  // entry_size
+            0x75, 0x72, 0x6C, 0x20,  // type 'url '
+            0x00, 0x00, 0x00, 0x01   // version(0) + flags
+        ]);
+
+        // Sound media header
+        constants.SMHD = new Uint8Array([
+            0x00, 0x00, 0x00, 0x00,  // version(0) + flags
+            0x00, 0x00, 0x00, 0x00   // balance(2) + reserved(2)
+        ]);
+
+        // video media header
+        constants.VMHD = new Uint8Array([
+            0x00, 0x00, 0x00, 0x01,  // version(0) + flags
+            0x00, 0x00,              // graphicsmode: 2 bytes
+            0x00, 0x00, 0x00, 0x00,  // opcolor: 3 * 2 bytes
+            0x00, 0x00
+        ]);
+    }
+
+    // Generate a box
+    static box(type) {
+        let size = 8;
+        let result = null;
+        let datas = Array.prototype.slice.call(arguments, 1);
+        let arrayCount = datas.length;
+
+        for (let i = 0; i < arrayCount; i++) {
+            size += datas[i].byteLength;
+        }
+
+        result = new Uint8Array(size);
+        result[0] = (size >>> 24) & 0xFF;  // size
+        result[1] = (size >>> 16) & 0xFF;
+        result[2] = (size >>>  8) & 0xFF;
+        result[3] = (size) & 0xFF;
+
+        result.set(type, 4);  // type
+
+        let offset = 8;
+        for (let i = 0; i < arrayCount; i++) {  // data body
+            result.set(datas[i], offset);
+            offset += datas[i].byteLength;
+        }
+
+        return result;
+    }
+
+    // emit ftyp & moov
+    static generateInitSegment(meta) {
+        let ftyp = MP4.box(MP4.types.ftyp, MP4.constants.FTYP);
+        let moov = MP4.moov(meta);
+
+        let result = new Uint8Array(ftyp.byteLength + moov.byteLength);
+        result.set(ftyp, 0);
+        result.set(moov, ftyp.byteLength);
+        return result;
+    }
+
+    // Movie metadata box
+    static moov(meta) {
+        let mvhd = MP4.mvhd(meta.timescale, meta.duration);
+        let trak = MP4.trak(meta);
+        let mvex = MP4.mvex(meta);
+        return MP4.box(MP4.types.moov, mvhd, trak, mvex);
+    }
+
+    // Movie header box
+    static mvhd(timescale, duration) {
+        return MP4.box(MP4.types.mvhd, new Uint8Array([
+            0x00, 0x00, 0x00, 0x00,  // version(0) + flags
+            0x00, 0x00, 0x00, 0x00,  // creation_time
+            0x00, 0x00, 0x00, 0x00,  // modification_time
+            (timescale >>> 24) & 0xFF,  // timescale: 4 bytes
+            (timescale >>> 16) & 0xFF,
+            (timescale >>>  8) & 0xFF,
+            (timescale) & 0xFF,
+            (duration >>> 24) & 0xFF,   // duration: 4 bytes
+            (duration >>> 16) & 0xFF,
+            (duration >>>  8) & 0xFF,
+            (duration) & 0xFF,
+            0x00, 0x01, 0x00, 0x00,  // Preferred rate: 1.0
+            0x01, 0x00, 0x00, 0x00,  // PreferredVolume(1.0, 2bytes) + reserved(2bytes)
+            0x00, 0x00, 0x00, 0x00,  // reserved: 4 + 4 bytes
+            0x00, 0x00, 0x00, 0x00,
+            0x00, 0x01, 0x00, 0x00,  // ----begin composition matrix----
+            0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00,
+            0x00, 0x01, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00,
+            0x40, 0x00, 0x00, 0x00,  // ----end composition matrix----
+            0x00, 0x00, 0x00, 0x00,  // ----begin pre_defined 6 * 4 bytes----
+            0x00, 0x00, 0x00, 0x00,
