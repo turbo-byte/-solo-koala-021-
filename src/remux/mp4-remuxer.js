e86f7c763ab030cffce12ed83fda93b18f9d14ea
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2022-09-08 15:09:14 +0000

    Update web-portfolio.html

--- src/remux/mp4-remuxer.js
+++ src/remux/mp4-remuxer.js
@@ -262,4 +262,12 @@ class MP4Remuxer {
         }
         if (samples.length === 1 && !force) {
             // If [sample count in current batch] === 1 && (force != true)
-            // Ignore and keep in demux
+            // Ignore and keep in demuxer's queue
+            return;
+        }  // else if (force === true) do remux
+
+        let offset = 0;
+        let mdatbox = null;
+        let mdatBytes = 0;
+
+        // calculate initial mdat s
