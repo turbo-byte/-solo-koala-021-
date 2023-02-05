1272dd6e2f7a853d5d4b0e177573266809cc3505
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2023-02-05 06:56:04 +0000

    msm: kgsl: Fix spinlock recursion in destroy pagetable

--- src/utils/typedarray-equality.ts
+++ src/utils/typedarray-equality.ts
@@ -14,4 +14,11 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- 
+ */
+
+function isAligned16(a: Uint8Array) : boolean {
+    return a.byteOffset % 2 === 0 && a.byteLength % 2 === 0;
+}
+
+function isAligned32(a: Uint8Array) : boolean {
+    return a.
