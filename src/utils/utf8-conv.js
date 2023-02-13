60429550a1fc50e434e0cb77cc8e763c2a27eb38
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2023-02-13 06:15:24 +0000

    Added translation using Weblate (Greek)

--- src/utils/utf8-conv.js
+++ src/utils/utf8-conv.js
@@ -1,2 +1,7 @@
 /*
- * Copyright (C) 2016 Bilibili. All Righ
+ * Copyright (C) 2016 Bilibili. All Rights Reserved.
+ *
+ * This file is derived from C++ project libWinTF8 (https://github.com/m13253/libWinTF8)
+ * @author zheng qian <xqq@xqq.im>
+ *
+ * Licensed under the Apache License, Version 2.0 (the
