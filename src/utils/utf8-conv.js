3ea6ea96b6fb652c1845a927357f9beb523df75c
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2023-02-13 07:21:16 +0000

    introduce SIZE_MAX

--- src/utils/utf8-conv.js
+++ src/utils/utf8-conv.js
@@ -4,4 +4,10 @@
  * This file is derived from C++ project libWinTF8 (https://github.com/m13253/libWinTF8)
  * @author zheng qian <xqq@xqq.im>
  *
- * Licensed under the Apache License, Version 2.0 (the
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *
