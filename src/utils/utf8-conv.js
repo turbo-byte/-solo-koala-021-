fdda20fc5bd87a520ecbf2fc797542ea24a78cfc
turbo-byte <91887767+turbo-byte@users.noreply.github.com>
2023-02-24 09:51:31 +0000

    validation added

--- src/utils/utf8-conv.js
+++ src/utils/utf8-conv.js
@@ -65,4 +65,7 @@ function decodeUTF8(uint8array) {
             if (checkContinuation(input, i, 3)) {
                 let ucs4 = (input[i] & 0x7) << 18 | (input[i + 1] & 0x3F) << 12
                          | (input[i + 2] & 0x3F) << 6 | (input[i + 3] & 0x3F);
-                if (ucs4 > 0x10000 && ucs4 < 0x110000
+                if (ucs4 > 0x10000 && ucs4 < 0x110000) {
+                    ucs4 -= 0x10000;
+                    out.push(String.fromCharCode((ucs4 >>> 10) | 0xD800));
+                    out.push(String.fromCh
